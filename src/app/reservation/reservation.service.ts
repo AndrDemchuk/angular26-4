480619283dbcebfc270020bb5ee06f5dd2f3762c
AndrDemchuk <xdemchuk@gmail.com>
2026-02-17 23:00:23 +0200

    local storage reservations save/update etc

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -6,6 +6,11 @@ import { Reservation } from "../models/reservation";
 export class ReservationService {
   private reservations: Reservation[] = [];
 
+  constructor() {
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
   //CRUD operations
   getReservations(): Reservation[] {
     return this.reservations;
@@ -17,16 +22,19 @@ export class ReservationService {
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
-    console.log(this.reservations);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex(res => res.id === id);
-    this.reservations.splice(index, 1)
+    this.reservations.splice(index, 1);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(updatedReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
     this.reservations[index] = updatedReservation;
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+
   }
 }
